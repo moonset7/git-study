a1588ee6cec5a1dff80b75dec29e281f3a3ffda8
moonset <alexmoonset7@gmail.com>
2024-12-16 00:41:15 -0800

    just a practice of commit without add

---
+++ git-xuexi2/src/main/java/com/example/gitxuexi2/GitXuexi2Application.java
@@ -0,0 +1,15 @@
+package com.example.gitxuexi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitXuexi2Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(GitXuexi2Application.class, args);
+    }
+
+}
---
+++ git-xuexi2/src/test/java/com/example/gitxuexi2/GitXuexi2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.gitxuexi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitXuexi2ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
